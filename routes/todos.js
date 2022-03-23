8e8a2523f2b7737bcd91716d905308722e57bb9a
GandavarapuTulasi-wal <tulasi.g@westagilelabs.com>
2022-03-23 23:49:08 +0530

    Added march 23 mongoose folder

--- routes/todos.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var todoController = require('../controllers/todo');
-var router = express.Router();
-router.get('/', todoController.getTodos);
-router.post('/', todoController.createTodo);
-router.post('/:_id', todoController.editTodo);
-router.delete('/:_id', todoController.deleteTodo);
-module.exports = router;
